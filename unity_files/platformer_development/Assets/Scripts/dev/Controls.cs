6c2dbcfbe96344ef3ed734bfe556b1998a7e77f4
Chase McNamara <ChasingAir@users.noreply.github.com>
2019-02-23 15:48:31 -0800

    Removed temp folders to dev
    Changed all temp folder names to dev. Temp folder is ignored for unity reasons and so everything I was working on wasn't being seen by github...

---
+++ unity_files/platformer_development/Assets/Scripts/dev/Controls.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float movespeed = 3f;
+    public bool moveright;
+    public bool moveleft;
+    public SpriteRenderer mySpriteRenderer;
+    public Joystick joystick;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update()
+    {
+        if(joystick.Horizontal > .5)
+        {
+        rb.velocity = new Vector2(movespeed, rb.velocity.y);
+        }
+        else if(joystick.Horizontal < -.5)
+        {
+        rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+        }
+        else
+        {
+          rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        //this stuff just flips the sprite for direction
+        if(joystick.Horizontal > 0)
+        {
+          mySpriteRenderer.flipX = false;
+        }
+        else if(joystick.Horizontal < 0)
+        {
+          mySpriteRenderer.flipX = true;
+        }
+/*
+        if (Input.GetKey(KeyCode.LeftArrow)) //if arrow key is pressed, set velocity equal to movespeed
+        {
+            rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) //if arrow key is pressed, set velocity equal to movespeed
+        {
+            rb.velocity = new Vector2(movespeed, rb.velocity.y);
+
+        }
+
+        if (moveright)
+        {
+            rb.velocity = new Vector2(movespeed, rb.velocity.y);
+        }
+        if (moveleft)
+        {
+            rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+        }
+        */
+    }
+}
---
+++ unity_files/platformer_development/Assets/Scripts/dev/old.Touch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch : MonoBehaviour
+{
+    private Controls player;
+
+
+    void Start()
+    {
+        player = FindObjectOfType<Controls>();
+    }
+
+    public void LeftArrow()
+    {
+        player.moveright = false;
+        player.moveleft = true;
+    }
+    public void RightArrow()
+    {
+        player.moveright = true;
+        player.moveleft = false;
+    }
+    public void ReleaseLeftArrow()
+    {
+        player.moveleft = false;
+    }
+    public void ReleaseRightArrow()
+    {
+        player.moveright = false;
+
+    }
+}
