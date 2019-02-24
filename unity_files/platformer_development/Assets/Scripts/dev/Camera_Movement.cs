69a0f410670d2b18a0f936af479e63986b6b2fb1
Chase McNamara <ChasingAir@users.noreply.github.com>
2019-02-23 17:03:51 -0800

    added pseudo scripts for touch & camera

---
+++ unity_files/platformer_development/Assets/Scripts/dev/Camera_Movement.cs
@@ -0,0 +1,27 @@
+// This script should move the camera smoothly towards the player, but lock the camera when there is any touch input
+
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //if(touching screen)
+      //{
+        //camera position: locked
+      //}
+      //else
+      //{
+        //move smoothly towards player
+      //}
+    }
+}
--- unity_files/platformer_development/Assets/Scripts/dev/Drag.cs
+++ unity_files/platformer_development/Assets/Scripts/dev/Drag.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+//This is the script for dragging objects. This needs to be written in such a way that it can be applied to any game object
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,12 +9,18 @@ public class Drag : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      //if(touching screen)
+      //{
+        //if(touching THIS_OBJECT)
+        //{
+          //move object same amount that finger moves
+        //}
+      //}
     }
 }
